3e06c306d8d53e18b3c1af6f6a3220bb540524bd
Vladislav <balenkov53@gmail.com>
2024-08-25 22:18:49 +0300

    update about url

--- homework_05/app.py
+++ homework_05/app.py
@@ -20,7 +20,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/about", endpoint='about')
+@app.route("/about/", endpoint='about')
 def about():
     return render_template('about.html')
 
